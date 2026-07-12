84a5693822187087a6e5a29a1c3ff4a12184be90
noah <ntholbro@uvm.edu>
2026-07-12 15:53:03 -0700

    continue on to explaining the hat matrix

--- derivation_from_bhat.py
+++ derivation_from_bhat.py
@@ -12,7 +12,10 @@ class DerivationFromBhat(StitcherScene):
         tex3 = MathTex(r"X^T Y = X^T X \hat{\beta}")
         tex4 = MathTex(r"(X^T X)^{-1} X^T Y = (X^T X)^{-1} X^T X \hat{\beta}")
         tex5 = MathTex(r"(X^T X)^{-1} X^T Y = \hat{\beta}")
-        tex6 = MathTex(r"\hat{\beta} = (X^T X)^{-1} X^T Y")
+        tex6 = MathTex(r"X (X^T X)^{-1} X^T Y = X \hat{\beta}")
+        tex7 = MathTex(r"X (X^T X)^{-1} X^T Y = \hat{Y}")
+        hm_formula  = MathTex(r"     X (X^T X)^{-1} X^T").move_to(tex7, aligned_edge=LEFT)
+        hm_formula2 = MathTex(r" H = X (X^T X)^{-1} X^T").next_to(hm_formula, DOWN, aligned_edge = RIGHT)
 
         with self.voiceover("So we can represent as X beta hat plus e. e is our error vector, and it is orthogonal to all the columns of X. We want to isolate beta hat. The first step in here is to") as tracker:
             self.play(Write(tex1))
@@ -32,3 +35,18 @@ class DerivationFromBhat(StitcherScene):
                                     ))
             self.play(TransformByGlyphMap(tex4, tex5,
                                           (range(11,21), FadeOut, {"run_time":0.5})))
+        
+        with self.voiceover("Now we can left multiply by X") as tracker:
+            self.play(TransformByGlyphMap(tex5, tex6,
+                                          (FadeIn, [0]),
+                                          (FadeIn, [12]),
+                                          ))
+        with self.voiceover("and X beta hat is Y hat.") as tracker:
+            self.play(TransformByGlyphMap(tex6, tex7,
+                                          ([12,13], [12])))
+        with self.voiceover("So this here is the hat matrix.") as tracker:
+            self.add(hm_formula)
+            hm_formula_box = SurroundingRectangle(hm_formula)
+            self.play(Create(hm_formula_box))
+            self.play(TransformByGlyphMap(hm_formula.copy(), hm_formula2,
+                                          (FadeIn, [0,1])))
