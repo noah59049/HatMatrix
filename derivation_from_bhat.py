a62df2522ad4bcd7b5b0963aad48852ca91d5461
noah <ntholbro@uvm.edu>
2026-07-12 15:39:26 -0700

    simplify and modify timings of voiceovers.

--- derivation_from_bhat.py
+++ derivation_from_bhat.py
@@ -14,18 +14,18 @@ class DerivationFromBhat(StitcherScene):
         tex5 = MathTex(r"(X^T X)^{-1} X^T Y = \hat{\beta}")
         tex6 = MathTex(r"\hat{\beta} = (X^T X)^{-1} X^T Y")
 
-        with self.voiceover("So we can represent as X beta hat plus e. e is our error vector, and it is orthogonal to all the columns of X.") as tracker:
+        with self.voiceover("So we can represent as X beta hat plus e. e is our error vector, and it is orthogonal to all the columns of X. We want to isolate beta hat. The first step in here is to") as tracker:
             self.play(Write(tex1))
-        with self.voiceover("We want to isolate beta hat. The first step in here is to multiply on the left by X transpose.") as tracker:
+        with self.voiceover("left multiply by X transpose. Since e is orthogonal to every column of X,") as tracker:
             self.play(TransformByGlyphMap(tex1, tex2,
                                           (FadeIn, [0,1]),
                                           (FadeIn, [4,5]),
                                           (FadeIn, [10,11]),
                                           ))
-        with self.voiceover("Since e is orthogonal to every column of X, X transpose e is zero.") as tracker:
+        with self.voiceover("X transpose e is zero. Now we have almost isolated beta hat. We") as tracker:
             self.play(TransformByGlyphMap(tex2, tex3,
                                           ([9,10,11,12], FadeOut, {"run_time": 0.5})))
-        with self.voiceover("Now we have almost isolated our beta hat. We left multiply by the inverse of X transpose X, and then we get our formula for beta hat. It's kind of amazing that there is a closed form solution for beta hat.") as tracker:
+        with self.voiceover("left multiply by the inverse of X transpose X, and then we get our formula for beta hat. It's kind of amazing it has a closed form solution.") as tracker:
             self.play(TransformByGlyphMap(tex3, tex4,
                                           (FadeIn, range(0,7)),
                                           (FadeIn, range(11,18), {"run_time": 0.45, "delay":0.5},),
