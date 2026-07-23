7a3f9c5f7ca468d7e0940e706d0a751a01c00f2d
noah <ntholbro@uvm.edu>
2026-07-23 13:32:36 -0700

    remove +1 and [1:] so we have a full grid, not one with the left and bottom edges cut off

--- X_span.py
+++ X_span.py
@@ -193,7 +193,7 @@ class XSpan(StitcherScene, ThreeDScene):
             # Do the sweeps
             # beta1_min, beta1_max = bhat_extremes(axes, X, np.array([0., 0.]), np.array([0., 1.]))
             grid_radius = 0.9
-            beta1_steps = np.linspace(-grid_radius, grid_radius, GRID_ROWS + 1)[1:]
+            beta1_steps = np.linspace(-grid_radius, grid_radius, GRID_ROWS)
             for beta1 in beta1_steps:
                 sweep_variable(
                     fixed_index=1, 
@@ -204,7 +204,7 @@ class XSpan(StitcherScene, ThreeDScene):
                 )
 
             # beta0_min, beta0_max = bhat_extremes(axes, X, np.array([0., 0.]), np.array([1., 0.]))
-            beta0_steps = np.linspace(-grid_radius, grid_radius, GRID_ROWS + 1)[1:]
+            beta0_steps = np.linspace(-grid_radius, grid_radius, GRID_ROWS)
             for beta0 in beta0_steps:
                 sweep_variable(
                     fixed_index=0, 
