68c5c22064be4af4a0b203d5ae80f4b5fee7dc9c
noah <ntholbro@uvm.edu>
2026-07-27 13:47:26 -0700

    move it more

--- X_span.py
+++ X_span.py
@@ -242,7 +242,7 @@ class XSpan(StitcherScene, ThreeDScene):
                 r"=",
                 numpy_to_latex(X),
                 numpy_to_latex(bhat.get_value())
-            ).move_to(RIGHT * 0.34)
+            ).move_to(RIGHT * 0.64)
             self.play(
                 TransformFromCopy(X_tex[0][0], xbhat_tex[0]),
                 TransformFromCopy(bhat_tex[0][0:2], xbhat_tex[1]),
