9a526779bb3815a95436f4b8d6f1109b94885513
noah <ntholbro@uvm.edu>
2026-08-07 22:02:40 -0700

    animate bhat to bhat_ols the graph starts spinning

--- X_span.py
+++ X_span.py
@@ -645,6 +645,11 @@ class XSpan(StitcherScene, ThreeDScene):
             self.play(FadeIn(residuals), run_time=self.get_current_voiceover_duration() / 3)
             graph_2d_group.add(residuals)
 
+            # Then set Y hat to be the OLS estimate: watch the residual
+            # squares shrink to their minimum total area as bhat gets there.
+            self.play(bhat.animate.set_value(bhat_ols), run_time=2 * self.get_current_voiceover_duration() / 3)
+
+            
             # The grid's viewing angle looks almost exactly down the plane's
             # normal (deliberately, so the grid itself isn't foreshortened) --
             # but that also means displacement *along* that normal, like how
@@ -678,9 +683,6 @@ class XSpan(StitcherScene, ThreeDScene):
 
             graph_group.add_updater(_spin)
 
-            # Then set Y hat to be the OLS estimate: watch the residual
-            # squares shrink to their minimum total area as bhat gets there.
-            self.play(bhat.animate.set_value(bhat_ols), run_time=2 * self.get_current_voiceover_duration() / 3)
         with self.voiceover("Now here's the key idea. This is the same as minimizing the Euclidean distance between Y and Y hat. And the length of the line between Y and Y hat is minimized precisely when that line is") as tracker:
             residual_3d_line = always_redraw(lambda: DashedLine(axes.c2p(*Y), axes.c2p(*yhat.get_value()), color=WHITE))
             graph_group.add(residual_3d_line)
