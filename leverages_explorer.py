081c9250060f0db5e158572bea9f9d4dac0f2e5b
noah <ntholbro@uvm.edu>
2026-08-30 22:58:48 -0700

    get the list comprehension working

--- leverages_explorer.py
+++ leverages_explorer.py
@@ -2,20 +2,17 @@ import numpy as np
 from manim import *
 from stitcher_scene import StitcherScene
 from leverages import X1, leverages
+from N_Tools import *
 
 
 class LeveragesExplorer(StitcherScene):
     def construct_scene(self):
         leverages_axes = Axes()
-        # leverages_plot = VGroup(
-        #     *[
-        #         Dot(axes.c2p(x, leverage) for x, leverage in zip(X1.flatten(), leverages.flatten()))
-        #     ]
-        # )
-        yeet = []
-        for x, leverage in zip(X1.flatten(), leverages.flatten()):
-            yeet.append(Dot(leverages_axes.c2p(x, leverage)))
-        leverages_plot = VGroup(*yeet)
+        leverages_plot = VGroup(
+            *[
+                Dot(leverages_axes.c2p(x, leverage)) for x, leverage in zip(X1.flatten(), leverages.flatten())
+            ]
+        )
         self.add(leverages_axes)
         self.add(leverages_plot)
         self.wait(2)
