359ef231e873b72cfc422170ce3538c8282e6bc7
noah <ntholbro@uvm.edu>
2026-09-16 15:06:02 -0700

    match up voiceovers with it

--- leverages_explorer.py
+++ leverages_explorer.py
@@ -174,6 +174,33 @@ class LeveragesExplorer(StitcherScene):
         ### Part 3: Play the scene ###
         ##############################
         # --- The actual scene being played ---
+        with self.voiceover("Now instead of the scatterplot of X and Y, we're going to just plot our X values; Y has no effect on leverages. And we're going to show") as tracker:
+            self.add(
+                x_axis,
+                x_plot,
+            )
+
+        with self.voiceover("a graph with X values on the X-axis and leverages on the Y-axis. This will show how leverages change with X.") as tracker:
+            self.add(
+                leverages_axes,
+                leverages_plot
+            )
+
+        with self.voiceover("Looks like the outlier is a high-leverage point, and all the others are low leverage. I wonder what happens if that outlier were") as tracker:
+            ...
+
+        with self.voiceover("closer to the distribution, say, if it were 2.5. Hmm, it looks like the leverages are making a curved shape. It kind of looks like a parabola. I wonder if it is a parabola. Let's") as tracker:
+            dot_dance(1)
+
+        with self.voiceover("fit a parabola to this and find out. That is a perfect fit to a parabola. Wow. What if we change the Xs to be") as tracker:
+            self.add(parabola_ink)
+
+        with self.voiceover("uniformly spaced? Still a parabola.") as tracker:
+            dot_dance(2)
+        
+        dot_dance(3)
+        dot_dance(4)
+        return
         self.add(
             x_axis,
             x_plot,
@@ -185,22 +212,3 @@ class LeveragesExplorer(StitcherScene):
             mean_line,
             a_label,
         )
-
-        for i in range(10):
-            dot_dance(i)
-            self.wait(1)
-
-        with self.voiceover("Now instead of the scatterplot of X and Y, we're going to just plot our X values; Y has no effect on leverages. And we're going to show a graph with X values on the X-axis and leverages on the Y-axis. This will show how leverages change with X.") as tracker:
-            ...
-
-        with self.voiceover("Looks like the outlier is a high-leverage point, and all the others are low leverage.") as tracker:
-            ...
-
-        with self.voiceover("I wonder what happens if that outlier were closer to the distribution, say, if it were 2.5") as tracker:
-            ...
-
-        with self.voiceover("Hmm, it looks like the leverages are making a curved shape. It kind of looks like a parabola. I wonder if it is a parabola. Let's fit a parabola to this and find out.") as tracker:
-            ...
-
-        with self.voiceover("That is a perfect fit to a parabola. Wow. What if we change the Xs to be uniformly spaced? Still a parabola.") as tracker:
-            ...
