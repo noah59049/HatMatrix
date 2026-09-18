8afdb52a40c8eb5c4e19a441dfd2010309093fc5
noah <ntholbro@uvm.edu>
2026-09-18 08:48:20 -0700

    eeeee

--- deprecated/z_equation.py
+++ deprecated/z_equation.py
@@ -12,3 +12,16 @@ class ZScene(Scene):
         self.play(TransformByGlyphMap(var_a_tex, a_var_tex,
                                       ([], [7]),
                                       ([0,1], [8,9])))
+        
+        vertex_forms = [
+            rf"y=a(x-h)^2+k",
+            rf"H_ii=a(x-h)^2+k",
+            rf"H_ii=a(x-\mu_X)^2+k",
+            rf"H_ii=a(x-\mu_X)^2+{1/n:.3f}",
+            rf"H_ii=a(x-\mu_X)^2+{1/n:.3f}",
+        ]
+
+        vertex_form_texes_list = [MathTex(v).to_edge(DOWN) for v in vertex_forms]
+        vertex_form_texes = VGroup(*vertex_form_texes_list)
+
+
